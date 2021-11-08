2e249f6e8036528f3c439b246522999c61c16bf7
Marcus Medina <mmedina@src.com>
2021-11-07 16:11:51 -0800

    Still Working

--- modules/Energy_or_Work.php
+++ modules/Energy_or_Work.php
@@ -30,19 +30,119 @@ class Energy_or_Work extends FormulaBase{
     function __construct(){
 
 		#{{{ Function Titles
-		$this->function_strings = array();
+		$this->function_strings = array(
+			1 => 'BTU to Foot-Pounds',
+			2 => 'Mega Joules to KiloWatt Hours',
+			3 => 'BTU to Gram-Calories',
+			4 => 'BTU to Joules',
+			5 => 'Joules to Watt Hours',
+			6 => 'Calories to Joules',
+			7 => 'Foot-Pounds to Joules',
+			8 => 'Joules to BTU',
+			9 => 'WattHours to Joules',
+			10 => 'Joules to Calories',
+			11 => 'Joules to Foot-Pounds',
+			12 => 'KiloWatt Hours to Mega Joules',
+		);
 		#}}}
 	
         #{{{ Function List
-		$this->function_list = array();
+		$this->function_list = array(
+			$this->function_strings[1] => function($num){
+				$result = $num * 778.2;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Foot/Pound'));
+			},
+			$this->function_strings[2] => function($num){
+				$result = $num * 0.27777778;
+				return array($this->prec($result, 4), $this->pluralize($result, 'KiloWatt Hour'));
+			},
+			$this->function_strings[3] => function($num){
+				$result = $num * 252;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Gram/Calorie'));
+			},
+			$this->function_strings[4] => function($num){
+				$result = $num * 1055.0559;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Joule'));
+			},
+			$this->function_strings[5] => function($num){
+				$result = $num * 0.00027777778;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Watt Hour'));
+			},
+			$this->function_strings[6] => function($num){
+				$result = $num * 4.184;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Joule'));
+			},
+			$this->function_strings[7] => function($num){
+				$result = $num * 1.3558;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Joule'));
+			},
+			$this->function_strings[8] => function($num){
+				$result = $num * 0.00094781712;
+				return array($this->prec($result, 4), $this->pluralize($result, 'BTU'));
+			},
+			$this->function_strings[9] => function($num){
+				$result = $num * 3600;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Joule'));
+			},
+			$this->function_strings[10] => function($num){
+				$result = $num * 0.23900574;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Calories'));
+			},
+			$this->function_strings[11] => function($num){
+				$result = $num * .7376;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Foot/Pound'));
+			},
+			$this->function_strings[12] => function($num){
+				$result = $num * 3.6;
+				return array($this->prec($result, 4), $this->pluralize($result, 'Mega Joule'));
+			},
+		);
         #}}}
 
         #{{{ Inputs
-		$this->functionInputs = array();
+		$this->functionInputs = array(
+			$this->function_strings[1] => array(
+				'number_input' => 'BTU (input): ',
+			),
+			$this->function_strings[2] => array(
+				'number_input' => 'Mega Joule (input): ',
+			),
+			$this->function_strings[3] => array(
+				'number_input' => 'BTU (input): ',
+			),
+			$this->function_strings[4] => array(
+				'number_input' => 'BTU (input): ',
+			),
+			$this->function_strings[5] => array(
+				'number_input' => 'Joule (input): ',
+			),
+			$this->function_strings[6] => array(
+				'number_input' => 'Calorie (input): ',
+			),
+			$this->function_strings[7] => array(
+				'number_input' => 'Foot/Pound (input): ',
+			),
+			$this->function_strings[8] => array(
+				'number_input' => 'Joule (input): ',
+			),
+			$this->function_strings[9] => array(
+				'number_input' => 'Watt Hour (input): ',
+			),
+			$this->function_strings[10] => array(
+				'number_input' => 'Joule (input): ',
+			),
+			$this->function_strings[11] => array(
+				'number_input' => 'Joule (input): ',
+			),
+			$this->function_strings[12] => array(
+				'number_input' => 'KiloWatt Hour (input): ',
+			),
+		);
         #}}}
 
         #{{{ Formula List
-		$this->formula_list = array();
+		$this->formula_list = array(
+		);
         #}}}
 
 	}
