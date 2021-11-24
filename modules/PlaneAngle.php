87f5858458e792516b87d8749622bf27d6a3bac5
Marcus Medina <mmedina@src.com>
2021-11-23 20:32:13 -0800

    conversion complete

--- modules/PlaneAngle.php
+++ modules/PlaneAngle.php
@@ -5,7 +5,7 @@
 #
 # Author: Marcus Medina
 # Date: Sat 06 Nov 2021 08:49:39 PM PDT
-# Last Update: 2021-11-22: 11:57
+# Last Update: 2021-11-23: 20:32
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -38,11 +38,66 @@ class PlaneAngle extends FormulaBase{
 			4 => 'Quadrants to Radians',
 			5 => 'Radians to Degrees',
 			6 => 'Minutes to Radians',
+			7 => 'Degree to Minute',
+			8 => 'Minute to Quadrant',
+			9 => 'Quadrant to Minute',
+			10 => 'Radian to Minute',
+			11 => 'Radian to Quadrant',
+			12 => 'Degree to Quadrant',
 		);
 		#}}}
 	
         #{{{ Function List
-		$this->function_list = array();
+		$this->function_list = array(
+			$this->function_strings[1] => function($num){
+				$result = $num * 0.017453293;
+				return array($result, $this->pluralize($result, 'Radian'));
+			},
+			$this->function_strings[2] => function($num){
+				$result = $num * 0.16666667;
+				return array($result, $this->pluralize($result, 'Degree'));
+			},
+			$this->function_strings[3] => function($num){
+				$result = $num * 90;
+				return array($result, $this->pluralize($result, 'Degree'));
+			},
+			$this->function_strings[4] => function($num){
+				$result = $num * 1.5707963;
+				return array($result, $this->pluralize($result, 'Radian'));
+			},
+			$this->function_strings[5] => function($num){
+				$result = $num * 57.29578;
+				return array($result, $this->pluralize($result, 'Degree'));
+			},
+			$this->function_strings[6] => function($num){
+				$result = $num * 0.00029088821;
+				return array($result, $this->pluralize($result, 'Radian'));
+			},
+			$this->function_strings[7] => function($num){
+				$result = $num * 60;
+				return array($result, $this->pluralize($result, 'Minute'));
+			},
+			$this->function_strings[8] => function($num){
+				$result = $num * 0.00018518519;
+				return array($result, $this->pluralize($result, 'Quadrant'));
+			},
+			$this->function_strings[9] => function($num){
+				$result = $num * 5400;
+				return array($result, $this->pluralize($result, 'Minute'));
+			},
+			$this->function_strings[10] => function($num){
+				$result = $num * 3437.7468;
+				return array($result, $this->pluralize($result, 'Minute'));
+			},
+			$this->function_strings[11] => function($num){
+				$result = $num * 0.63661977;
+				return array($result, $this->pluralize($result, 'Quadrant'));
+			},
+			$this->function_strings[12] => function($num){
+				$result = $num * 0.011111111;
+				return array($result, $this->pluralize($result, 'Quadrant'));
+			},
+		);
         #}}}
 
         #{{{ Inputs
@@ -64,6 +119,24 @@ class PlaneAngle extends FormulaBase{
 			),
 			$this->function_strings[6] => array(
 				'number_input' => 'Minutes (input): ',
+			),
+			$this->function_strings[7] => array(
+				'number_input' => 'Degree (input): ',
+			),
+			$this->function_strings[8] => array(
+				'number_input' => 'Minute (input): ',
+			),
+			$this->function_strings[9] => array(
+				'number_input' => 'Quadrant (input): ',
+			),
+			$this->function_strings[10] => array(
+				'number_input' => 'Radian (input): ',
+			),
+			$this->function_strings[11] => array(
+				'number_input' => 'Radian (input): ',
+			),
+			$this->function_strings[12] => array(
+				'number_input' => 'Degree (input): ',
 			),
 		);
         #}}}
@@ -88,8 +161,26 @@ class PlaneAngle extends FormulaBase{
 			$this->function_strings[6] => array(
 				'Formula:<br>' => 'Minute * 0.00029088821'
 			),
+			$this->function_strings[7] => array(
+				'Formula:<br>' => 'Degree * 60'
+			),
+			$this->function_strings[8] => array(
+				'Formula:<br>' => 'Minute * 0.00018518519'
+			),
+			$this->function_strings[9] => array(
+				'Formula:<br>' => 'Quadrant * 5400'
+			),
+			$this->function_strings[10] => array(
+				'Formula:<br>' => 'Radian * 3437.7468'
+			),
+			$this->function_strings[11] => array(
+				'Formula:<br>' => 'Radian * 0.63661977'
+			),
+			$this->function_strings[12] => array(
+				'Formula:<br>' => 'Degree * 0.011111111'
+			),
 		);
         #}}}
-
 	}
 }
+
