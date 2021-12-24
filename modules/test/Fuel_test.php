2ced6d3a7c96fb98696f452152080b9fe569a5b2
Marcus Medina <mmedina@src.com>
2021-12-23 19:42:20 -0800

    Test Complete

--- modules/test/Fuel_test.php
+++ modules/test/Fuel_test.php
@@ -5,7 +5,7 @@
 #
 # Author: Marcus Medina
 # Date: Fri 17 Dec 2021 12:41:16 PM PST
-# Last Update: 2021-12-17: 13:05
+# Last Update: 2021-12-23: 19:07
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,23 +27,25 @@
 
 require_once("pass_fail.php");
 require_once("imports.inc.php");
+$test = new TestSuite();
 
 function test_Fuel(){
 	global $fuel;
+	global $test;
 	echo "\nFuel Economy
 -------------------------------\n";
 	if ( $fuel->function_list[$fuel->function_strings[1]](234) !== array(234*0.42514371, "Kilometers/Liter")){
-		set_fail($fuel->function_strings[1]);
+		$test->set_fail($fuel->function_strings[1]);
 	} else {
-		set_pass($fuel->function_strings[1]);
+		$test->set_pass($fuel->function_strings[1]);
 	}
 	if ( $fuel->function_list[$fuel->function_strings[2]](234) !== array(234*2.3521458, "Miles/Gallon")){
-		set_fail($fuel->function_strings[2]);
+		$test->set_fail($fuel->function_strings[2]);
 	} else {
-		set_pass($fuel->function_strings[2]);
+		$test->set_pass($fuel->function_strings[2]);
 	}
 }
 
 echo "Preparing Fuel Economy...";
 sleep(1);
-pass_fail($fuel, test_Fuel());
+$test->pass_fail($fuel, test_Fuel());
