c79de6a1fd8084ddf9d17d07d04ceb7984c8e2ff
Marcus Medina <mmedina@src.com>
2022-02-17 10:57:28 -0800

    Converting Resistive Inductive Parallel Test

--- imports.inc.php
+++ imports.inc.php
@@ -5,7 +5,7 @@
 #
 # Author: Marcus Medina
 # Date: Fri 08 Oct 2021 01:33:05 PM PDT
-# Last Update: 2022-02-17: 08:57
+# Last Update: 2022-02-17: 10:47
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -105,6 +105,7 @@ $cats = array(
     'Torque' => $tor,
     'Velocity' => $vel,
     'Resistive Capacitance Parallel' => $rcp,
+    'Resistive Capacitance Series' => $rcs,
     'Resistive Inductive Parallel' => $rip,
 
 );
--- modules/test/Resist_Induc_Par_test.php
+++ modules/test/Resist_Induc_Par_test.php
@@ -5,7 +5,7 @@
 #
 # Author: Marcus Medina
 # Date: Fri 17 Dec 2021 12:49:52 PM PST
-# Last Update: 2022-02-17: 07:48
+# Last Update: 2022-02-17: 10:57
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -33,11 +33,46 @@ $test = new TestSuite();
 function Resist_Induct_Par_test($test, $class){
     $fail = 0;
     $pass = 0;
-    if ( $class->function_list[$class->function_strings[1]](234,234) !== array(1/((1/pow(234,2))+(1/pow(234,2))), "Impedance")){
+    if ( $class->function_list[$class->function_strings[1]](234,234) !== array($class->prec(1/((1/pow(234,2))+(1/pow(234,2))), 4), "Impedance")){
         $test->set_fail($class->function_strings[1]);
         $fail++;
     } else {
         $test->set_pass($class->function_strings[1]);
+        $pass++;
+    }
+    if ( $class->function_list[$class->function_strings[2]](234,234) !== array($class->prec(234/234, 4), "Impedance")){
+        $test->set_fail($class->function_strings[2]);
+        $fail++;
+    } else {
+        $test->set_pass($class->function_strings[2]);
+        $pass++;
+    }
+    if ( $class->function_list[$class->function_strings[3]](234,234) !== array($class->prec(pow(234, 2)/234, 4), "Impedance")){
+        $test->set_fail($class->function_strings[3]);
+        $fail++;
+    } else {
+        $test->set_pass($class->function_strings[3]);
+        $pass++;
+    }
+    if ( $class->function_list[$class->function_strings[4]](234,234) !== array($class->prec(234/pow(234,2),4), "Impedance")){
+        $test->set_fail($class->function_strings[4]);
+        $fail++;
+    } else {
+        $test->set_pass($class->function_strings[4]);
+        $pass++;
+    }
+    if ( $class->function_list[$class->function_strings[5]](234,234) !== array($class->prec(234*234, 4), "Impedance")){
+        $test->set_fail($class->function_strings[5]);
+        $fail++;
+    } else {
+        $test->set_pass($class->function_strings[5]);
+        $pass++;
+    }
+    if ( $class->function_list[$class->function_strings[6]](234,234) !== array($class->prec(2*$class->PI*234*234, 4), "Inductive Reactance")){
+        $test->set_fail($class->function_strings[6]);
+        $fail++;
+    } else {
+        $test->set_pass($class->function_strings[6]);
         $pass++;
     }
     echo "\nTested: ".count($class->function_list);
