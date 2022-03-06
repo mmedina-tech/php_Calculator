7c1c2a81f536f07e91753a98c5eacb9e15e0bd76
Marcus Medina <mmedina@src.com>
2022-03-05 18:36:27 -0800

    Mass refactor finished

--- modules/Mass.Module.php
+++ modules/Mass.Module.php
@@ -5,7 +5,7 @@
 #
 # Author: Marcus Medina
 # Date: Sat 06 Nov 2021 08:47:30 PM PDT
-# Last Update: 2022-03-02: 20:29
+# Last Update: 2022-03-05: 18:36
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,104 +29,99 @@ require_once ("FormulaBase.php");
 
 class Mass extends FormulaBase{
     function __construct(){
-
-        #{{{ Function Titles
-        $this->function_strings = array(
-            1 => 'Tons to Kilograms',
-            2 => 'Kilograms to Tons',
-            3 => 'Tons to Metric Tons',
-            4 => 'Metric Tons to Tons',
-            5 => 'Grains to Drams',
-            6 => 'Grains to Ounces',
-            7 => 'Grains to Grams',
-        );
-        #}}}
-
-        #{{{ Function List
-        $this->function_list = array(
-            $this->function_strings[1] => function($num){
-                $result = $num * 907.18474;
-                return array($result, $this->pluralize($result, 'Kilogram'));
-            },
-            $this->function_strings[2] => function($num){
-                $result = $num * 0.0011023113;
-                return array($result, $this->pluralize($result, 'Ton'));
-            },
-            $this->function_strings[3] => function($num){
-                $result = $num * 0.90718474;
-                return array($result, $this->pluralize($result, 'Metric Ton'));
-            },
-            $this->function_strings[4] => function($num){
-                $result = $num * 1.1023113;
-                return array($result, $this->pluralize($result, 'Ton'));
-            },
-            $this->function_strings[5] => function($num){
-                $result = $num * 0.036571429;
-                return array($result, $this->pluralize($result, 'Dram'));
-            },
-            $this->function_strings[6] => function($num){
-                $result = $num * 0.0022857143;
-                return array($result, $this->pluralize($result, 'Ounce'));
-            },
-            $this->function_strings[7] => function($num){
-                $result = $num * 0.06479891;
-                return array($result, $this->pluralize($result, 'Gram'));
-            },
-        );
-        #}}}
-
-        #{{{ Inputs
-        $this->function_inputs = array(
-            $this->function_strings[1] => array(
-                'number_input' => 'Tons (input): ',
+        $this->entries = array(
+            1 => FormulaBase::create_func_entry(
+                'Tons to Kilograms',
+                function($num){
+                    $result = $num * 907.18474;
+                    return array($result, $this->pluralize($result, 'Kilogram'));
+                },
+                array(
+                    'number_input' => 'Tons (input): ',
+                ),
+                array(
+                    '' => 'Tons * 907.18474'
+                ),
             ),
-            $this->function_strings[2] => array(
-                'number_input' => 'Kilograms (input): ',
+            FormulaBase::create_func_entry(
+                'Kilograms to Tons',
+                function($num){
+                    $result = $num * 0.0011023113;
+                    return array($result, $this->pluralize($result, 'Ton'));
+                },
+                array(
+                    'number_input' => 'Kilograms (input): ',
+                ),
+                array(
+                    '' => 'Kilograms * 0.0011023113'
+                ),
             ),
-            $this->function_strings[3] => array(
-                'number_input' => 'Tons (input): ',
+            FormulaBase::create_func_entry(
+                'Tons to Metric Tons',
+                function($num){
+                    $result = $num * 0.90718474;
+                    return array($result, $this->pluralize($result, 'Metric Ton'));
+                },
+                array(
+                    'number_input' => 'Tons (input): ',
+                ),
+                array(
+                    '' => 'Tons * 0.90718474'
+                ),
             ),
-            $this->function_strings[4] => array(
-                'number_input' => 'Metric Tons (input): ',
+            FormulaBase::create_func_entry(
+                'Metric Tons to Tons',
+                function($num){
+                    $result = $num * 1.1023113;
+                    return array($result, $this->pluralize($result, 'Ton'));
+                },
+                array(
+                    'number_input' => 'Metric Tons (input): ',
+                ),
+                array(
+                    '' => 'Metric Tons * 1.1023113'
+                ),
             ),
-            $this->function_strings[5] => array(
-                'number_input' => 'Grains (input): ',
+            FormulaBase::create_func_entry(
+                'Grains to Drams',
+                function($num){
+                    $result = $num * 0.036571429;
+                    return array($result, $this->pluralize($result, 'Dram'));
+                },
+                array(
+                    'number_input' => 'Grains (input): ',
+                ),
+                array(
+                    '' => 'Grains * 0.036571429'
+                ),
             ),
-            $this->function_strings[6] => array(
-                'number_input' => 'Grains (input): ',
+            FormulaBase::create_func_entry(
+                'Grains to Ounces',
+                function($num){
+                    $result = $num * 0.0022857143;
+                    return array($result, $this->pluralize($result, 'Ounce'));
+                },
+                array(
+                    'number_input' => 'Grains (input): ',
+                ),
+                array(
+                    '' => 'Grains * 0.0022857143'
+                ),
             ),
-            $this->function_strings[7] => array(
-                'number_input' => 'Grains (input): ',
-            ),
-        );
-        #}}}
-
-        #{{{ Formula List
-        $this->formula_list = array(
-            $this->function_strings[1] => array(
-                'Formula:<br>' => 'Tons * 907.18474'
-            ),
-            $this->function_strings[2] => array(
-                'Formula:<br>' => 'Kilograms * 0.0011023113'
-            ),
-            $this->function_strings[3] => array(
-                'Formula:<br>' => 'Tons * 0.90718474'
-            ),
-            $this->function_strings[4] => array(
-                'Formula:<br>' => 'Metric Tons * 1.1023113'
-            ),
-            $this->function_strings[5] => array(
-                'Formula:<br>' => 'Grains * 0.036571429'
-            ),
-            $this->function_strings[6] => array(
-                'Formula:<br>' => 'Grains * 0.0022857143'
-            ),
-            $this->function_strings[7] => array(
-                'Formula:<br>' => 'Grains * 0.06479891'
+            FormulaBase::create_func_entry(
+                'Grains to Grams',
+                function($num){
+                    $result = $num * 0.06479891;
+                    return array($result, $this->pluralize($result, 'Gram'));
+                },
+                array(
+                    'number_input' => 'Grains (input): ',
+                ),
+                array(
+                    '' => 'Grains * 0.06479891'
+                ),
             ),
         );
-        #}}}
-
     }
 }
 $cats["Mass"] = new Mass();
