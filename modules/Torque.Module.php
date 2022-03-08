98ce21ac0a475bf1105e554c0b330fbf8dfa9819
Marcus Medina <mmedina@src.com>
2022-03-08 07:08:23 -0800

    finished Torque

--- modules/Torque.Module.php
+++ modules/Torque.Module.php
@@ -5,7 +5,7 @@
 #
 # Author: Marcus Medina
 # Date: Sat 06 Nov 2021 09:00:03 PM PDT
-# Last Update: 2022-03-02: 20:35
+# Last Update: 2022-03-08: 07:08
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,93 +29,86 @@ require_once ("FormulaBase.php");
 
 class Torque extends FormulaBase{
     function __construct(){
-
-        #{{{ Function Titles
-        $this->function_strings = array(
-            1 => 'Gram-Centimeters to Ounce-Inches',
-            2 => 'Newton-Meters to Pound-Feet',
-            3 => 'Newton-Meters to Pound-Inches',
-            4 => 'Ounce-Inches to Gram-Centimeters',
-            5 => 'Pound-Feet to Newton-Meters',
-            6 => 'Pound-Inches to Newton-Meters',
-        );
-        #}}}
-
-        #{{{ Function List
-        $this->function_list = array(
-            $this->function_strings[1] => function($num){
-                $result = $num * 0.089595863;
-                return array($result, $this->pluralize($result, 'Ounce/Inch'));
-            },
-            $this->function_strings[2] => function($num){
-                $result = $num * 0.068521766;
-                return array($result, $this->pluralize($result, 'Pound/Foot'));
-            },
-            $this->function_strings[3] => function($num){
-                $result = $num * 0.0057101472;
-                return array($result, $this->pluralize($result, 'Pound/Inch'));
-            },
-            $this->function_strings[4] => function($num){
-                $result = $num * 11.16123;
-                return array($result, $this->pluralize($result, 'Gram/Centimeter'));
-            },
-            $this->function_strings[5] => function($num){
-                $result = $num * 14.593903;
-                return array($result, $this->pluralize($result, 'Newton/Meter'));
-            },
-            $this->function_strings[6] => function($num){
-                $result = $num * 175.12684;
-                return array($result, $this->pluralize($result, 'Newton/Meter'));
-            },
-        );
-        #}}}
-
-        #{{{ Inputs
-        $this->function_inputs = array(
-            $this->function_strings[1] => array(
-                'number_input' => 'Gram/Centimeter (input): ',
+        $this->entries = array(
+            1 => FormulaBase::create_func_entry(
+                'Gram-Centimeters to Ounce-Inches',
+                function($num){
+                    $result = $num * 0.089595863;
+                    return array($result, $this->pluralize($result, 'Ounce/Inch'));
+                },
+                array(
+                    'number_input' => 'Gram/Centimeter (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Gram/Centimeter * 0.089595863'
+                ),
             ),
-            $this->function_strings[2] => array(
-                'number_input' => 'Newton/Meter (input): ',
+            FormulaBase::create_func_entry(
+                'Newton-Meters to Pound-Feet',
+                function($num){
+                    $result = $num * 0.068521766;
+                    return array($result, $this->pluralize($result, 'Pound/Foot'));
+                },
+                array(
+                    'number_input' => 'Newton/Meter (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Newton/Meter * 0.068521766'
+                ),
             ),
-            $this->function_strings[3] => array(
-                'number_input' => 'Newton/Meters (input): ',
+            FormulaBase::create_func_entry(
+                'Newton-Meters to Pound-Inches',
+                function($num){
+                    $result = $num * 0.0057101472;
+                    return array($result, $this->pluralize($result, 'Pound/Inch'));
+                },
+                array(
+                    'number_input' => 'Newton/Meters (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Newton/Meter * 0.0057101472'
+                ),
             ),
-            $this->function_strings[4] => array(
-                'number_input' => 'Ounce/Inch (input): ',
+            FormulaBase::create_func_entry(
+                'Ounce-Inches to Gram-Centimeters',
+                function($num){
+                    $result = $num * 11.16123;
+                    return array($result, $this->pluralize($result, 'Gram/Centimeter'));
+                },
+                array(
+                    'number_input' => 'Ounce/Inch (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Ounce/Inch * 11.16123'
+                ),
             ),
-            $this->function_strings[5] => array(
-                'number_input' => 'Pound/Foot (input): ',
+            FormulaBase::create_func_entry(
+                'Pound-Feet to Newton-Meters',
+                function($num){
+                    $result = $num * 14.593903;
+                    return array($result, $this->pluralize($result, 'Newton/Meter'));
+                },
+                array(
+                    'number_input' => 'Pound/Foot (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Pound/Foot * 14.593903'
+                ),
             ),
-            $this->function_strings[6] => array(
-                'number_input' => 'Pound/Inch (input): ',
-            ),
-        );
-        #}}}
-
-        #{{{ Formula List
-        $this->formula_list = array(
-            $this->function_strings[1] => array(
-                'Formula:<br>' => 'Gram/Centimeter * 0.089595863'
-            ),
-            $this->function_strings[2] => array(
-                'Formula:<br>' => 'Newton/Meter * 0.068521766'
-            ),
-            $this->function_strings[3] => array(
-                'Formula:<br>' => 'Newton/Meter * 0.0057101472'
-            ),
-            $this->function_strings[4] => array(
-                'Formula:<br>' => 'Ounce/Inch * 11.16123'
-            ),
-            $this->function_strings[5] => array(
-                'Formula:<br>' => 'Pound/Foot * 14.593903'
-            ),
-            $this->function_strings[6] => array(
-                'Formula:<br>' => 'Pound/Inch * 175.12684'
+            FormulaBase::create_func_entry(
+                'Pound-Inches to Newton-Meters',
+                function($num){
+                    $result = $num * 175.12684;
+                    return array($result, $this->pluralize($result, 'Newton/Meter'));
+                },
+                array(
+                    'number_input' => 'Pound/Inch (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Pound/Inch * 175.12684'
+                ),
             ),
         );
-        #}}}
-
     }
 }
 $cats["Torque"] = new Torque();
