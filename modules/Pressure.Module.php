64a6c5ee9316da51a5128a6fa574b32724e920f3
Marcus Medina <mmedina@src.com>
2022-03-08 06:56:30 -0800

    finished Pressure

--- modules/Pressure.Module.php
+++ modules/Pressure.Module.php
@@ -5,7 +5,7 @@
 #
 # Author: Marcus Medina
 # Date: Sat 06 Nov 2021 08:50:36 PM PDT
-# Last Update: 2022-03-02: 20:32
+# Last Update: 2022-03-08: 06:56
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,138 +29,138 @@ require_once ("FormulaBase.php");
 
 class Pressure extends FormulaBase{
     function __construct(){
-
-        #{{{ Function Titles
-        $this->function_strings = array(
-            1 => 'Bars to KiloPascals',
-            2 => 'Bars to PSI',
-            3 => 'Inches of Mercury to KiloPascals',
-            4 => 'Inches of Water to KiloPascals',
-            5 => 'KiloPascals to Bars',
-            6 => 'KiloPascals to Inches of Mercury',
-            7 => 'KiloPascals to PSI',
-            8 => 'Pascals to Pounds per Square Foot',
-            9 => 'Pounds per Square Foor to Pascals',
-            10 => 'PSI to KiloPascals',
+        $this->entries = array(
+            1 => FormulaBase::create_func_entry(
+                'Bars to KiloPascals',
+                function($num){
+                    $result = $num * 100;
+                    return array($result, $this->pluralize($result, 'KiloPascal'));
+                },
+                array(
+                    'number_input' => 'Bars (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Bars * 100'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'Bars to PSI',
+                function($num){
+                    $result = $num * 14.503774;
+                    return array($result, $this->pluralize($result, 'PSI'));
+                },
+                array(
+                    'number_input' => 'Bars (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Bars * 14.503774'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'Inches of Mercury to KiloPascals',
+                function($num){
+                    $result = $num * 3.377;
+                    return array($result, $this->pluralize($result, 'KiloPascal'));
+                },
+                array(
+                    'number_input' => 'Inches of Mercury (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Inches of Mercury * 3.377'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'Inches of Water to KiloPascals',
+                function($num){
+                    $result = $num * 0.2488;
+                    return array($result, $this->pluralize($result, 'KiloPascal'));
+                },
+                array(
+                    'number_input' => 'Inches of Water (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Inches of Water * 0.2488'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'KiloPascals to Bars',
+                function($num){
+                    $result = $num * 0.01;
+                    return array($result, $this->pluralize($result, 'Bar'));
+                },
+                array(
+                    'number_input' => 'KiloPascals (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'KiloPascal * 0.01'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'KiloPascals to Inches of Mercury',
+                function($num){
+                    $result = $num * 0.2961;
+                    return array($result, $this->pluralize($result, 'Inch of Mercury'));
+                },
+                array(
+                    'number_input' => 'KiloPascals (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'KiloPascal * 0.2961'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'KiloPascals to PSI',
+                function($num){
+                    $result = $num * 0.14503774;
+                    return array($result, $this->pluralize($result, 'PSI'));
+                },
+                array(
+                    'number_input' => 'KiloPascals (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'KiloPascal * 0.14503774'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'Pascals to Pounds per Square Foot',
+                function($num){
+                    $result = $num * 0.020885434;
+                    return array($result, $this->pluralize($result, 'Pound/Foot<sup>2</sup>'));
+                },
+                array(
+                    'number_input' => 'Pascals (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Pascal * 0.020885434'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'Pounds per Square Foor to Pascals',
+                function($num){
+                    $result = $num * 47.880259;
+                    return array($result, $this->pluralize($result, 'Pascal'));
+                },
+                array(
+                    'number_input' => 'Pounds/Foot<sup>2</sup> (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'Pound/Foot<sup>2</sup> * 47.880259'
+                ),
+            ),
+            FormulaBase::create_func_entry(
+                'PSI to KiloPascals',
+                function($num){
+                    $result = $num * 6.8947573;
+                    return array($result, $this->pluralize($result, 'KiloPascal'));
+                },
+                array(
+                    'number_input' => 'PSI (input): ',
+                ),
+                array(
+                    'Formula:<br>' => 'PSI * 6.8947573'
+                ),
+            ),
         );
-        #}}}
-
-        #{{{ Function List
-        $this->function_list = array(
-            $this->function_strings[1] => function($num){
-                $result = $num * 100;
-                return array($result, $this->pluralize($result, 'KiloPascal'));
-            },
-            $this->function_strings[2] => function($num){
-                $result = $num * 14.503774;
-                return array($result, $this->pluralize($result, 'PSI'));
-            },
-            $this->function_strings[3] => function($num){
-                $result = $num * 3.377;
-                return array($result, $this->pluralize($result, 'KiloPascal'));
-            },
-            $this->function_strings[4] => function($num){
-                $result = $num * 0.2488;
-                return array($result, $this->pluralize($result, 'KiloPascal'));
-            },
-            $this->function_strings[5] => function($num){
-                $result = $num * 0.01;
-                return array($result, $this->pluralize($result, 'Bar'));
-            },
-            $this->function_strings[6] => function($num){
-                $result = $num * 0.2961;
-                return array($result, $this->pluralize($result, 'Inch of Mercury'));
-            },
-            $this->function_strings[7] => function($num){
-                $result = $num * 0.14503774;
-                return array($result, $this->pluralize($result, 'PSI'));
-            },
-            $this->function_strings[8] => function($num){
-                $result = $num * 0.020885434;
-                return array($result, $this->pluralize($result, 'Pound/Foot<sup>2</sup>'));
-            },
-            $this->function_strings[9] => function($num){
-                $result = $num * 47.880259;
-                return array($result, $this->pluralize($result, 'Pascal'));
-            },
-            $this->function_strings[10] => function($num){
-                $result = $num * 6.8947573;
-                return array($result, $this->pluralize($result, 'KiloPascal'));
-            },
-        );
-        #}}}
-
-        #{{{ Inputs
-        $this->function_inputs = array(
-            $this->function_strings[1] => array(
-                'number_input' => 'Bars (input): ',
-            ),
-            $this->function_strings[2] => array(
-                'number_input' => 'Bars (input): ',
-            ),
-            $this->function_strings[3] => array(
-                'number_input' => 'Inches of Mercury (input): ',
-            ),
-            $this->function_strings[4] => array(
-                'number_input' => 'Inches of Water (input): ',
-            ),
-            $this->function_strings[5] => array(
-                'number_input' => 'KiloPascals (input): ',
-            ),
-            $this->function_strings[6] => array(
-                'number_input' => 'KiloPascals (input): ',
-            ),
-            $this->function_strings[7] => array(
-                'number_input' => 'KiloPascals (input): ',
-            ),
-            $this->function_strings[8] => array(
-                'number_input' => 'Pascals (input): ',
-            ),
-            $this->function_strings[9] => array(
-                'number_input' => 'Pounds/Foot<sup>2</sup> (input): ',
-            ),
-            $this->function_strings[10] => array(
-                'number_input' => 'PSI (input): ',
-            ),
-        );
-        #}}}
-
-        #{{{ Formula List
-        $this->formula_list = array(
-            $this->function_strings[1] => array(
-                'Formula:<br>' => 'Bars * 100'
-            ),
-            $this->function_strings[2] => array(
-                'Formula:<br>' => 'Bars * 14.503774'
-            ),
-            $this->function_strings[3] => array(
-                'Formula:<br>' => 'Inches of Mercury * 3.377'
-            ),
-            $this->function_strings[4] => array(
-                'Formula:<br>' => 'Inches of Water * 0.2488'
-            ),
-            $this->function_strings[5] => array(
-                'Formula:<br>' => 'KiloPascal * 0.01'
-            ),
-            $this->function_strings[6] => array(
-                'Formula:<br>' => 'KiloPascal * 0.2961'
-            ),
-            $this->function_strings[7] => array(
-                'Formula:<br>' => 'KiloPascal * 0.14503774'
-            ),
-            $this->function_strings[8] => array(
-                'Formula:<br>' => 'Pascal * 0.020885434'
-            ),
-            $this->function_strings[9] => array(
-                'Formula:<br>' => 'Pound/Foot<sup>2</sup> * 47.880259'
-            ),
-            $this->function_strings[10] => array(
-                'Formula:<br>' => 'PSI * 6.8947573'
-            ),
-
-        );
-        #}}}
-
     }
 }
 $cats["Pressure"] = new Pressure();
